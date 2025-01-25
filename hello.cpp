49727181f19c49700920fdd5ac92de3c38649b09
draconewman <62671853+draconewman@users.noreply.github.com>
2025-01-25 22:16:57 +0530

    cpp default array

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,12 @@
 // #include<bits/stdc++.h>
 #include<iostream>
+#include <vector>
 using namespace std;
 
 int main(){
-    int x;
-    cin>>x;
-    cout<<"hello "<<x;
+    vector<int> ans(5); // Initialize with 5 elements
+    for (int x : ans) {
+        cout << x << " "; // Outputs: 0 0 0 0 0
+    }
+    return 0;
 }
